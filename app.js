701fa31d32d3733402ded1fabf31e4103b64cac3
Tanishq Singhai <tanishqsinghai11@gmail.com>
2020-05-24 23:38:24 +0530

    fix

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "https: //radiant-ocean-98732.herokuapp.com/auth/google/secrets",
+        callbackURL: "https://radiant-ocean-98732.herokuapp.com/auth/google/secrets",
         userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     function(accessToken, refreshToken, profile, cb) {
@@ -73,7 +73,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
-        callbackURL: "http://:3000/auth/facebook/secrets"
+        callbackURL: "http://radiant-ocean-98732.herokuapp.com/auth/facebook/secrets"
     },
     function(accessToken, refreshToken, profile, cb) {
         User.findOrCreate({ facebookId: profile.id }, function(err, user) {
